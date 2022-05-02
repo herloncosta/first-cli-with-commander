14eece7b3950dfb51d88dae09ead09009dd9ba18
herloncosta <herlon36@gmail.com>
2022-05-01 21:22:34 -0300

    es modules

--- src/mac-vendor-finder.js
+++ src/mac-vendor-finder.js
@@ -1,5 +1,7 @@
-const { program } = require("commander");
-const axios = require("axios");
+import { program } from "commander";
+import axios from "axios";
+
+main();
 
 async function getVendor(mac) {
     try {
@@ -10,22 +12,23 @@ async function getVendor(mac) {
     }
 }
 
-program
-    .name("mac-vendor-finder")
-    .version("1.0.0")
-    .description("Search mac vendor.")
-    .option("-m, --mac", "mac address | format=<FC:A1:3E:2A:1C:33>")
-    .action(() => {
-        const options = program.opts();
-        try {
-            const mac = program.args[0].split(":").join("-");
-            getVendor(mac);
-        } catch (err) {
-            console.error(err.name, err.type, "no mac inserted");
-        }
-    });
+function main() {
+    program
+        .name("mac-vendor-finder")
+        .version("1.0.0")
+        .description("Search mac vendor.")
+        .option("-m, --mac", "mac address | format=<FC:A1:3E:2A:1C:33>")
+        .action(() => {
+            const options = program.opts();
+            try {
+                const mac = program.args[0].split(":").join("-");
+                getVendor(mac);
+            } catch (err) {
+                console.error(err.name, err.type, "no mac inserted");
+            }
+        });
 
-program.parse();
-
+    program.parse();
+}
 // mac example: 00:1B:C9:4B:E3:57
 // api: https://api.macvendors.com/
